193e79874f005b324061024f2a3287bfb74c707c
stardustrain <kthanterran@gmail.com>
2020-11-01 23:05:29 +0900

    Add env.ts

---
+++ env.ts
@@ -0,0 +1,5 @@
+export const VIEW_MODE = Deno.env.get("VIEW_MODE")
+export const GITHUB_TOKEN = Deno.env.get("GITHUB_TOKEN")
+// NOTE: Format YYYY-MM-DD
+export const START_AT = Deno.env.get('START_AT') ?? ''
+export const END_AT = Deno.env.get('END_AT') ?? ''
